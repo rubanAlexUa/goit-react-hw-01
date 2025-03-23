f7e1950e223f0f9aa64b58d9c8b24c09de03767a
Alex <113380721+rubanAlexUa@users.noreply.github.com>
2025-03-23 11:56:39 +0200

    add changes

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,19 +1,5 @@
-import c from "./FriendList.module.css";
+import FriendListItem from "./FriendListItem";
 
 export default function Friends({ friends }) {
-  const friendsList = friends.map((friend) => (
-    <div className={c.friend_item} key={friend.id}>
-      <img
-        className={c.friends_avatar}
-        src={friend.avatar}
-        alt="Avatar"
-        width="48"
-      />
-      <p className={c.friends_name}>{friend.name}</p>
-      <p className={friend.isOnline ? c.online : c.offline}>
-        {friend.isOnline ? "Online" : "Offline"}
-      </p>
-    </div>
-  ));
-  return <div className={c.friends_container}>{friendsList}</div>;
+  return <FriendListItem friends={friends} />;
 }
---
+++ src/components/FriendListItem.jsx
@@ -0,0 +1,21 @@
+import c from "./FriendListItem.module.css";
+
+export default function FriendListItem({ friends }) {
+  const friendsList = friends.map((friend) => (
+    <li key={friend.id}>
+      <div className={c.friend_item}>
+        <img
+          className={c.friends_avatar}
+          src={friend.avatar}
+          alt="Avatar"
+          width="48"
+        />
+        <p className={c.friends_name}>{friend.name}</p>
+        <p className={friend.isOnline ? c.online : c.offline}>
+          {friend.isOnline ? "Online" : "Offline"}
+        </p>
+      </div>
+    </li>
+  ));
+  return <ul className={c.friends_container}>{friendsList}</ul>;
+}
